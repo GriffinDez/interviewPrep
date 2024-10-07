699785fc1c635b11e2d5bc9cc720239afce6d2b4
Aawni_K <griffin.kode@gmail.com>
2024-10-07 15:23:40 +0000

    02_traversalInArray

---
+++ 01_Basics.md
@@ -0,0 +1,27 @@
+## Null Vs Undefined
+
+## Function Vs Block Scope
+
+## Automatic SemiColon Insertion
+
+## Rest Vs Spread Operator
+
+## NaN
+
+## Arrow Function
+
+## closure
+
+## Explain sum(5)(6) = 11;
+
+## iterables and iterators
+
+## Generators
+
+## MemoryManagement & GarbageCollection
+
+## How to handle Errors in javaScript code.
+
+
+
+
---
+++ 02_Array.js
@@ -0,0 +1,57 @@
+//. Explain Traversal in Array
+//> Array, where we can store the multiple values under one name.
+//> classname of array, 'Array'
+let A_arr = [1, 2, 3]
+let A_arr1 = ['Hi', true, 456]
+console.log(A_arr);
+console.log(A_arr1[0]);
+console.log(A_arr.length);
+console.log(A_arr[A_arr.length - 1]);
+
+let A_arr2 = [
+    { pCode: 1, pName: 'Apple' },
+    { pCode: 2, pName: 'Banana' },
+    { pCode: 3, pName: 'C' }
+]
+console.log(A_arr2[1].pName);   // need to pick the property from which index of an array.
+
+let arr = ['ES5', 'ES6', 'ES7'];
+for (let i = 0; i < arr.length; i++) {
+    const ele = arr[i];
+    console.log('forLoop---');
+    console.log(ele);
+}
+
+//?> ================================================================================
+
+//. Ways to loop through an array
+
+
+//?> ================================================================================
+
+//. Add, Remove, Insert, Replace elements in Array
+
+//?> ================================================================================
+
+//-> Search Operation in Array
+//> 
+
+//?> ================================================================================
+
+
+//. use of map() method..?
+
+//?> ================================================================================
+
+
+//. How to flaten 2D array
+
+//?> ================================================================================
+
+
+//. how to sort an Array
+
+//?> ================================================================================
+
+
+//. Array Destructuring
