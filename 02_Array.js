7a868aadc8495c33d4cdf176b5f5f16071887489
Aawni_K <griffin.kode@gmail.com>
2024-10-08 10:01:42 +0000

    02_SearchOperation on an Array

--- 02_Array.js
+++ 02_Array.js
@@ -74,7 +74,47 @@ console.log(arrSplice.splice(0));   // deletes all the ele from Index 0. returns
 //?> ================================================================================
 
 //-> Search Operation in Array
-//> 
+let searchArr = ['A','B', 'C','A','D','A']
+//= indexOf()
+//> arrName.indexOf(searchEle,[startIndex])
+//> startIndex. default = 0
+//> Returns the indexNo.of the ele where the item is found. 
+//> If noSearchEle in array, returns -1.
+//> if more than one match, returns the index of the 1st item found.
+console.log(searchArr.indexOf('A'));    // 0, returns the index of the 1st occurance of the ele.
+console.log(searchArr.indexOf('A', 2)); // 3, starts the searching from index 2.
+console.log(searchArr.indexOf('a'));    // -1, indexOf() is caseSenstive.
+
+
+//= lastIndexOf()
+//> returns the last index of the ele found. If nothing found returns -1.
+//> byDefault, startIndex = array.length - 1
+console.log(searchArr.lastIndexOf('A'));    //5, lastIndexOf() starts searching from (array.length -1). i.e from the end.
+console.log(searchArr.lastIndexOf('A',2));    //0, starts the search from backwards. i.e. the end of array.
+
+//= includes()
+//> returns boolean value, 
+console.log(searchArr.includes('B',1));   // searches from index1 & returns true/False 
+
+//. Conditional Search
+let conSearchArr = [1000, 2000, 3000, 4000, 5000, 2500]
+//= find()
+//> takes a callback function, which is called for each ele.
+//> have 4arguments, such as element, index, array OnWhich the opertion is performed, thisArg.
+//> Returns the value of the 1st element in the array OR "undefined" if nothing found.
+//> finds only 1val at a time.. Drawback
+console.log(conSearchArr.find((ele, index, arr, thisArg)=>{return ele>2000}));  //3000, returns the 1st val which satisfies the condition.
+
+//= findIndex()
+//> returns the 1st index of the ele which satisfies the condition.
+//> returns "undefined", if noValue found.
+//> finds only 1value at a time.. Drawback
+console.log(conSearchArr.findIndex((ele, index, arr, thisArg)=>{return ele>2000})); // 2, works same as find(), but returns the index instead of value.
+
+//= filter()
+//> returns the new Array of elements which satisfies the condition.
+//> if nothing found, returns an empty Array. [].
+console.log(conSearchArr.filter((ele, index, arr, thisArg)=>{return ele>2000})); // returns the new Array of ele which satisfies the condition rather than the single value.
 
 //?> ================================================================================
 
